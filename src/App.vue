45b0c549e37c68d1a69bdca0c600443779bcfed5
David Martinez <davidmgilo@gmail.com>
2017-01-13 16:27:41 +0100

    list 2

--- src/App.vue
+++ src/App.vue
@@ -17,19 +17,11 @@
 
         <md-list>
           <md-list-item>
-            <md-icon>move_to_inbox</md-icon> <span>Inbox</span>
+            <md-icon>move_to_inbox</md-icon> <span>Todos</span>
           </md-list-item>
 
           <md-list-item>
-            <md-icon>send</md-icon> <span>Sent Mail</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>delete</md-icon> <span>Trash</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>error</md-icon> <span>Spam</span>
+            <md-icon>send</md-icon> <span>Tokens</span>
 
             <md-divider class="md-inset"></md-divider>
           </md-list-item>
@@ -46,29 +38,6 @@
             </md-button>
           </md-list-item>
 
-          <md-list-item>
-            <md-avatar>
-              <img src="https://placeimg.com/40/40/people/1" alt="People">
-            </md-avatar>
-
-            <span>Alex Nelson</span>
-
-            <md-button class="md-icon-button md-list-action">
-              <md-icon class="md-primary">chat_bubble</md-icon>
-            </md-button>
-          </md-list-item>
-
-          <md-list-item>
-            <md-avatar>
-              <img src="https://placeimg.com/40/40/people/6" alt="People">
-            </md-avatar>
-
-            <span>Mary Johnson</span>
-
-            <md-button class="md-icon-button md-list-action">
-              <md-icon>chat_bubble</md-icon>
-            </md-button>
-          </md-list-item>
         </md-list>
       </md-sidenav>
     </div>
