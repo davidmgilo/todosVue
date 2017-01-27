60d78f7c6494e68f75d50918b6baf33f8a579061
David Martinez <davidmgilo@gmail.com>
2017-01-27 15:54:33 +0100

    card in profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>
-        Profile Todo
-    </div>
+    <md-card md-with-hover>
+        <md-card-header>
+            <md-avatar>
+                <img src="assets/avatar.png" alt="People">
+            </md-avatar>
+
+            <div class="md-title">David Martinez Gilo</div>
+            <div class="md-subhead">davidmgilo</div>
+        </md-card-header>
+
+        <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea nostrum.
+        </md-card-content>
+
+        <md-card-actions>
+            <md-button>Action</md-button>
+            <md-button>Action</md-button>
+        </md-card-actions>
+    </md-card>
 </template>
 <style>
 </style>
