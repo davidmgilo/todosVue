53e2c3c010d80fb3a1741460862a869feecbf41b
David Martinez <davidmgilo@gmail.com>
2017-03-17 17:29:23 +0100

    device

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>
-       TODO device info here!
-    </div>
+    <form novalidate @submit.stop.prevent="submit">
+        <md-input-container>
+            <label>Model</label>
+            <md-input v-model="model" readonly></md-input>
+        </md-input-container>
+    </form>
 </template>
 <style>
 </style>
 <script>
+  export default{
+    data () {
+      return {
+        model: ''
+      }
+    },
+    created () {
+      document.addEventListener('deviceready', this.onDeviceReady, false)
+    },
+    methods: {
+      onDeviceReady () {
+        this.model = window.device.model
+      }
+    }
+  }
 </script>
