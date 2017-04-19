b662c771c2c09b2a098044e4924ecba1af060a05
David Martínez Gilo <davidmgilo@gmail.com>
2017-04-19 07:49:10 +0200

    build

--- src/todosVue.js
+++ src/todosVue.js
@@ -1,7 +1,8 @@
 const todosVue = {
   STORAGE_KEY: 'todosvue_token',
   AUTH_CLIENT_ID: '1',
-  AUTH_REDIRECT_URI: 'http://localhost:8095/login',
+  // AUTH_REDIRECT_URI: 'http://localhost:8095/login',
+  AUTH_REDIRECT_URI: 'http://todosvue.davidmartinez.2dam.acacha.org/login',
   API_PROFILE_URL: 'http://todosbackend.davidmartinez.2dam.acacha.org:8080/api/v1/loguser',
   ANDROID_SENDER_ID: '1066055111450',
   REGISTER_GCM_TOKEN_URL: 'http://todosbackend.davidmartinez.2dam.acacha.org:8080/api/v1/user/gcmtoken',
