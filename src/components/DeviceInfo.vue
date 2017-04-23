2008449012a34be185ab40308570e9d47505afc9
David Martínez Gilo <davidmgilo@gmail.com>
2017-04-23 23:15:16 +0200

    added more info on device info

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -4,6 +4,30 @@
             <label>Model</label>
             <md-input v-model="model" readonly></md-input>
         </md-input-container>
+        <md-input-container>
+            <label>Platform</label>
+            <md-input v-model="platform" readonly></md-input>
+        </md-input-container>
+        <md-input-container>
+            <label>Uuid</label>
+            <md-input v-model="uuid" readonly></md-input>
+        </md-input-container>
+        <md-input-container>
+            <label>Version</label>
+            <md-input v-model="version" readonly></md-input>
+        </md-input-container>
+        <md-input-container>
+            <label>Manufacturer</label>
+            <md-input v-model="manufacturer" readonly></md-input>
+        </md-input-container>
+        <md-input-container>
+            <label>Is it Virtual?</label>
+            <md-input v-model="isVirtual" readonly></md-input>
+        </md-input-container>
+        <md-input-container>
+            <label>Serial</label>
+            <md-input v-model="serial" readonly></md-input>
+        </md-input-container>
     </form>
 </template>
 <style>
@@ -12,7 +36,13 @@
   export default{
     data () {
       return {
-        model: ''
+        model: '',
+        platform: '',
+        uuid: '',
+        version: '',
+        manufacturer: '',
+        isVirtual: '',
+        serial: ''
       }
     },
     created () {
@@ -21,6 +51,12 @@
     methods: {
       onDeviceReady () {
         this.model = window.device.model
+        this.platform = window.device.platform
+        this.uuid = window.device.uuid
+        this.version = window.device.version
+        this.manufacturer = window.device.manufacturer
+        this.isVirtual = window.device.isVirtual
+        this.serial = window.device.serial
       }
     }
   }
